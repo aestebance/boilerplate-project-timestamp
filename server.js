22867e8f3ea5458e6b85ab6da68b8fa70c3504b4
Alejandro <aesteance@uoc.edu>
2021-01-09 02:30:46 +0100

    changing

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.get("/api/timestamp/:date", function(req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(80, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
